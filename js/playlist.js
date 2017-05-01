521712080f502912b7a69d12f00c6e7313cfe10f
benjaminGovehovitch <benjamin.govehovitch@gmail.com>
2017-05-01 19:20:10 +0200

    initial commit

---
+++ js/playlist.js
@@ -0,0 +1,117 @@
+// HTML5 audio player + playlist controls...
+// Inspiration: http://jonhall.info/how_to/create_a_playlist_for_html5_audio
+// Mythium Archive: https://archive.org/details/mythium/
+
+
+jQuery(function ($) {
+    var supportsAudio = !!document.createElement('audio').canPlayType;
+    if (supportsAudio) {
+        var index = 0,
+            playing = false,
+            mediaPath = 'https://archive.org/download/Symphonie_fantastique_201704/',
+            extension = '',
+                        tracks = [{
+                "track": 1,
+                "name": "Reveries Passions",
+                "length": "13:59",
+                "file": "1-Reveries_Passions"
+            }, {
+                "track": 2,
+                "name": "Un bal",
+                "length": "6:26",
+                "file": "2-Un_bal"
+            }, {
+                "track": 3,
+                "name": "Scene aux champs",
+                "length": "15:00",
+                "file": "3-Scene_aux_champs"
+            }, {
+                "track": 4,
+                "name": "Marche au supplice",
+                "length": "4:29",
+                "file": "4-Marche_au_supplice"
+            }, {
+                "track": 5,
+                "name": "Songe d'une nuit du Sabbat",
+                "length": "9:17",
+                "file": "5-Songe_d_une_nuit_du_sabbat"
+            },],
+            buildPlaylist = $.each(tracks, function(key, value) {
+                var trackNumber = value.track,
+                    trackName = value.name,
+                    trackLength = value.length;
+                if (trackNumber.toString().length === 1) {
+                    trackNumber = '0' + trackNumber;
+                } else {
+                    trackNumber = '' + trackNumber;
+                }
+                $('#plList').append('<li><div class="plItem"><div class="plNum">' + trackNumber + '.</div><div class="plTitle">' + trackName + '</div><div class="plLength">' + trackLength + '</div></div></li>');
+            }),
+            trackCount = tracks.length,
+            npAction = $('#npAction'),
+            npTitle = $('#npTitle'),
+            audio = $('#audio1').bind('play', function () {
+                playing = true;
+                npAction.text('Lecture');
+            }).bind('pause', function () {
+                playing = false;
+                npAction.text('Pause');
+            }).bind('ended', function () {
+                npAction.text('Pause');
+                if ((index + 1) < trackCount) {
+                    index++;
+                    loadTrack(index);
+                    audio.play();
+                } else {
+                    audio.pause();
+                    index = 0;
+                    loadTrack(index);
+                }
+            }).get(0),
+            btnPrev = $('#btnPrev').click(function () {
+                if ((index - 1) > -1) {
+                    index--;
+                    loadTrack(index);
+                    if (playing) {
+                        audio.play();
+                    }
+                } else {
+                    audio.pause();
+                    index = 0;
+                    loadTrack(index);
+                }
+            }),
+            btnNext = $('#btnNext').click(function () {
+                if ((index + 1) < trackCount) {
+                    index++;
+                    loadTrack(index);
+                    if (playing) {
+                        audio.play();
+                    }
+                } else {
+                    audio.pause();
+                    index = 0;
+                    loadTrack(index);
+                }
+            }),
+            li = $('#plList li').click(function () {
+                var id = parseInt($(this).index());
+                if (id !== index) {
+                    playTrack(id);
+                }
+            }),
+            loadTrack = function (id) {
+                $('.plSel').removeClass('plSel');
+                $('#plList li:eq(' + id + ')').addClass('plSel');
+                npTitle.text(tracks[id].name);
+                index = id;
+                audio.src = mediaPath + tracks[id].file + extension;
+            },
+            playTrack = function (id) {
+                loadTrack(id);
+                audio.play();
+            };
+        extension = audio.canPlayType('audio/mpeg') ? '.mp3' : audio.canPlayType('audio/ogg') ? '.ogg' : '';
+        loadTrack(index);
+    }
+});
