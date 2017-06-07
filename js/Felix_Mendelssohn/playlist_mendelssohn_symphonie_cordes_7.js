0e49cf4b7f521ab8cb78f308e5f8768166344dcd
benjaminGovehovitch <benjamin.govehovitch@gmail.com>
2017-06-08 00:05:02 +0200

    symphonie cordes Mendelssohn

---
+++ js/Felix_Mendelssohn/playlist_mendelssohn_symphonie_cordes_7.js
@@ -0,0 +1,112 @@
+// HTML5 audio player + playlist controls...
+// Inspiration: http://jonhall.info/how_to/create_a_playlist_for_html5_audio
+// Mythium Archive: https://archive.org/details/mythium/
+
+
+jQuery(function ($) {
+    var supportsAudio = !!document.createElement('audio').canPlayType;
+    if (supportsAudio) {
+        var index = 0,
+            playing = false,
+            mediaPath = 'https://archive.org/download/Mendelssohn_Symphonie_cordes_7/',
+            extension = '',
+                        tracks = [{
+                "track": 1,
+                "name": "Allegro",
+                "length": "5:31",
+                "file": "01_Allegro"
+            }, {
+                "track": 2,
+                "name": "Andante (amorevole)",
+                "length": "5:33",
+                "file": "02_Andante_amorevole"
+            }, {
+                "track": 3,
+                "name": "Menuetto",
+                "length": "3:48",
+                "file": "03_Menuetto"
+            }, {
+                "track": 4,
+                "name": "Allegro molto",
+                "length": "4:59",
+                "file": "04_Allegro_molto"
+            },],
+            buildPlaylist = $.each(tracks, function(key, value) {
+                var trackNumber = value.track,
+                    trackName = value.name,
+                    trackLength = value.length;
+                if (trackNumber.toString().length === 1) {
+                    trackNumber = '0' + trackNumber;
+                } else {
+                    trackNumber = '' + trackNumber;
+                }
+                $('#plList').append('<li><div class="plItem"><div class="plNum">' + trackNumber + '.</div><div class="plTitle">' + trackName + '</div><div class="plLength">' + trackLength + '</div></div></li>');
+            }),
+            trackCount = tracks.length,
+            npAction = $('#npAction'),
+            npTitle = $('#npTitle'),
+            audio = $('#audio1').bind('play', function () {
+                playing = true;
+                npAction.text('Lecture');
+            }).bind('pause', function () {
+                playing = false;
+                npAction.text('Pause');
+            }).bind('ended', function () {
+                npAction.text('Pause');
+                if ((index + 1) < trackCount) {
+                    index++;
+                    loadTrack(index);
+                    audio.play();
+                } else {
+                    audio.pause();
+                    index = 0;
+                    loadTrack(index);
+                }
+            }).get(0),
+            btnPrev = $('#btnPrev').click(function () {
+                if ((index - 1) > -1) {
+                    index--;
+                    loadTrack(index);
+                    if (playing) {
+                        audio.play();
+                    }
+                } else {
+                    audio.pause();
+                    index = 0;
+                    loadTrack(index);
+                }
+            }),
+            btnNext = $('#btnNext').click(function () {
+                if ((index + 1) < trackCount) {
+                    index++;
+                    loadTrack(index);
+                    if (playing) {
+                        audio.play();
+                    }
+                } else {
+                    audio.pause();
+                    index = 0;
+                    loadTrack(index);
+                }
+            }),
+            li = $('#plList li').click(function () {
+                var id = parseInt($(this).index());
+                if (id !== index) {
+                    playTrack(id);
+                }
+            }),
+            loadTrack = function (id) {
+                $('.plSel').removeClass('plSel');
+                $('#plList li:eq(' + id + ')').addClass('plSel');
+                npTitle.text(tracks[id].name);
+                index = id;
+                audio.src = mediaPath + tracks[id].file + extension;
+            },
+            playTrack = function (id) {
+                loadTrack(id);
+                audio.play();
+            };
+        extension = audio.canPlayType('audio/mpeg') ? '.mp3' : audio.canPlayType('audio/ogg') ? '.ogg' : '';
+        loadTrack(index);
+    }
+});
